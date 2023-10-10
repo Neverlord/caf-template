93bcf234df6f0f375896acbcaf0dd66a745a6438
Dominik Charousset <dominik@charousset.de>
2023-10-10 20:04:39 +0200

    Initial commit

---
+++ example-8.cpp
@@ -0,0 +1,11 @@
+#include "caf/actor_system.hpp"
+#include "caf/actor_system_config.hpp"
+#include "caf/caf_main.hpp"
+
+#include <cstdlib>
+
+int caf_main(caf::actor_system&) {
+  return EXIT_SUCCESS;
+}
+
+CAF_MAIN()
---
+++ example-9.test.cpp
@@ -0,0 +1,4 @@
+#include "caf/test/test.hpp"
+#include "caf/test/caf_test_main.hpp"
+
+CAF_TEST_MAIN()
